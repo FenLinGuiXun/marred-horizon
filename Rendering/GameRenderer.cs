2bfc94079d08f053cdd4c08ebf014b61c8346176
焚林桂熏 <fenlinguixun@proton.me>
2026-08-01 19:38:52 -0500

    Connect GPU render texture to display

--- Rendering/GameRenderer.cs
+++ Rendering/GameRenderer.cs
@@ -4,9 +4,24 @@ public partial class GameRenderer : Node3D
 {
 	private TextureRect _renderOutput;
 	
+	private RenderingDevice _device;
+	private RenderResources _resources;
+	private Texture2Drd _displayTexture;
+	
 	public override void _Ready()
 	{
 		_renderOutput = GetNode<TextureRect>("../Display/RenderOutput");
-		GD.Print("Render output connected");
+
+		_device = RenderingServer.GetRenderingDevice();
+		_resources = new RenderResources(_device);
+
+		_displayTexture = new Texture2Drd
+		{
+			TextureRdRid = _resources.ColorTexture
+		};
+
+		_renderOutput.Texture = _displayTexture;
+
+		GD.Print("GPU texture connected");
 	}
 }
